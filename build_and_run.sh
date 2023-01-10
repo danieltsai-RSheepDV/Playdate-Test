dadd0d66a8f2f42388448ed92f2083a84a1c17ec
Daniel Tsai <32005886+danieltsai-RSheepDV@users.noreply.github.com>
2023-01-09 23:02:54 -0800

    to you

---
+++ build_and_run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+pdc Source Builds/Game.pdx
+cd Temp
+
+# if [$sim]
+# then
+#     $sim.CloseMainWindow()
+#     $count = 0
+#     while (!$sim.HasExited) 
+#     {
+#         Start-Sleep -Milliseconds 10
+#         $count += 1
+
+#         if ($count -ge 5)
+#         {
+#             $sim | Stop-Process -Force
+#         }
+#     }
+# fi
+if [[ $OSTYPE == 'darwin'* ]]; then
+    open "$HOME/Developer/PlaydateSDK/bin/Playdate Simulator.app" ../Builds/Game.pdx
+fi 
+cd ..
